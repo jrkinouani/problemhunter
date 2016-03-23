f65aca802eba359e53341802cc4b97bd4ca0e25a
jrkinouani <kinouani@gmail.com>
2016-03-23 09:44:13 +0400

    controller static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+  
   devise_for :users
 
-  root to: "devise#index"
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
